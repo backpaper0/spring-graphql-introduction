726b7679b1e8fedf62b6cd1969f5d53f2654d89a
Uragami Taichi <backpaper0@gmail.com>
2021-08-08 21:31:51 +0900

    GraphiQLのパスが変更したことに対応していなかった

--- src/main/java/com/example/security/WebSecurityConfig.java
+++ src/main/java/com/example/security/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.antMatchers("/graphql/**").permitAll()
-				.antMatchers("/graphiql/**").permitAll()
+				.antMatchers("/my-graphiql/**").permitAll()
 				.anyRequest().authenticated()
 
 				.and().httpBasic()
