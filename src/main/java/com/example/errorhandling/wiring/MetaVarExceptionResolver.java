58590f6c308c90446bc1b52a0a067dfc4615124d
Uragami Taichi <backpaper0@gmail.com>
2021-10-16 17:38:45 +0900

    Polish

--- src/main/java/com/example/errorhandling/wiring/MetaVarExceptionResolver.java
+++ src/main/java/com/example/errorhandling/wiring/MetaVarExceptionResolver.java
@@ -1,32 +1,28 @@
 package com.example.errorhandling.wiring;
 
-import java.util.List;
-
-import org.springframework.graphql.execution.DataFetcherExceptionResolver;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
 import org.springframework.stereotype.Component;
 
 import graphql.GraphQLContext;
 import graphql.GraphQLError;
 import graphql.GraphqlErrorBuilder;
 import graphql.schema.DataFetchingEnvironment;
-import reactor.core.publisher.Mono;
 
 @Component
-public class MetaVarExceptionResolver implements DataFetcherExceptionResolver {
+public class MetaVarExceptionResolver extends DataFetcherExceptionResolverAdapter {
 
 	@Override
-	public Mono<List<GraphQLError>> resolveException(Throwable exception, DataFetchingEnvironment environment) {
-		GraphQLContext context = environment.getGraphQlContext();
+	protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+		GraphQLContext context = env.getGraphQlContext();
 		if (context != null) {
 			Boolean handling = context.get("handling");
 
 			if (Boolean.TRUE.equals(handling)) {
-				GraphQLError error = GraphqlErrorBuilder.newError(environment)
-						.message("Handled: %s", exception.getMessage()).build();
-				List<GraphQLError> errors = List.of(error);
-				return Mono.just(errors);
+				GraphQLError error = GraphqlErrorBuilder.newError(env)
+						.message("Handled: %s", ex.getMessage()).build();
+				return error;
 			}
 		}
-		return Mono.empty();
+		return null;
 	}
 }
